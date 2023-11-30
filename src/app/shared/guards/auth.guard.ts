3c85f562605ed81cc5be90ef29aaa7122a467256
Humberto Longoria <79929899+HumbertL@users.noreply.github.com>
2023-11-29 21:08:27 -0600

    Completed Guards

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,5 +1,28 @@
 import { CanActivateFn } from '@angular/router';
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenService } from '../services/token.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private tokenService: TokenService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    // const tokenService: TokenService = inject(TokenService);
+    // const router: Router = inject(Router);
+    if(this.tokenService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+  
+}
--- src/app/shared/guards/unauth.guard.ts
+++ src/app/shared/guards/unauth.guard.ts
@@ -1,5 +1,24 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenService } from '../services/token.service';
 
-export const unauthGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class UnauthGuard implements CanActivate {
+
+  constructor(private tokenService: TokenService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(!this.tokenService.isLoggedIn()) {
+        return true;
+      } else {
+        this.router.navigate(['']);
+        return false;
+      }
+  }
+  
+}
